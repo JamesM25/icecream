1604f7e74fb028d47a24552bb67e6ef013a68f6d
James <motherwell.james@student.greenriver.edu>
2023-04-11 13:43:13 -0400

    Add header include and order summary

---
+++ process.php
@@ -0,0 +1,46 @@
+<?php
+/*
+ * James Motherwell
+ * 4/11/2024
+ * 328/icecream/process.php
+ * Process order for ice cream shop
+ */
+
+// Turn on error reporting
+ini_set("display_errors", 1);
+error_reporting(E_ALL);
+
+// Define constants
+define("PRICE_PER_SCOOP", 2.00);
+define("SALES_TAX", 0.08);
+
+// Include header
+include "header.html";
+?>
+
+<body>
+<div class="container">
+    <h1>Thank you for your order!</h1>
+    <?php
+//        echo "<pre>";
+//        var_dump($_POST);
+//        echo "</pre>";
+
+        // Get data from post array
+        $cone = $_POST["cone"];
+        $numScoops = $_POST["scoops"];
+        $flavors = $_POST["flavor"];
+        $flavorList = implode(", ", $flavors);
+
+        $subTotal = $numScoops * PRICE_PER_SCOOP;
+        $total = $subTotal + $subTotal * SALES_TAX;
+
+        echo "<p>Cone: $cone</p>";
+        echo "<p>Number of scoops: $numScoops</p>";
+        echo "<p>Flavors: $flavorList</p>";
+
+        echo "<p>Subtotal: $" . number_format($subTotal, 2) . "</p>";
+        echo "<p>Total: $" . number_format($total, 2) . "</p>";
+    ?>
+</div>
+</body>
